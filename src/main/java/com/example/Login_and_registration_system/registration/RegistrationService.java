a719c22898801aa917ea13f863c900868b06a904
Dawid Badora <dawidbadora@gmail.com>
2022-10-17 02:02:46 +0200

    User registration and security

---
+++ src/main/java/com/example/Login_and_registration_system/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.example.Login_and_registration_system.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
