32034033b19f7be2f097a0e1583689e370a79200
Dawid Badora <dawidbadora@gmail.com>
2022-10-19 19:47:25 +0200

    amendments

--- src/main/java/com/example/Login_and_registration_system/registration/RegistrationService.java
+++ src/main/java/com/example/Login_and_registration_system/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.example.Login_and_registration_system.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
