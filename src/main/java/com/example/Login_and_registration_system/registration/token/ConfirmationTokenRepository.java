3248de7334dfc866f0801ea75f200484a3279347
Dawid Badora <dawidbadora@gmail.com>
2022-11-04 21:54:18 +0100

    Making token 1/2

---
+++ src/main/java/com/example/Login_and_registration_system/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.Login_and_registration_system.registration.token;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ConfirmationTokenRepository {
+}
---
+++ src/main/java/com/example/Login_and_registration_system/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,4 @@
+package com.example.Login_and_registration_system.registration.token;
+
+public class ConfirmationTokenService {
+}
